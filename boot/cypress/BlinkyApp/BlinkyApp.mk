3823be9e75d5e5afe72fce4ecd6222d81e14bf4d
Bohdan Kovalchuk <bohd@cypress.com>
2020-04-01 02:07:38 -0500

    1159: fixing BlinkyApp 1M RAM as well

--- boot/cypress/BlinkyApp/BlinkyApp.mk
+++ boot/cypress/BlinkyApp/BlinkyApp.mk
@@ -99,7 +99,7 @@ endif
 
 ifeq ($(PLATFORM), PSOC_064_1M)
 	# Set RAM start and size
-	DEFINES_APP += -DRAM_START=0x08010000
+	DEFINES_APP += -DRAM_START=0x08020000
 	DEFINES_APP += -DRAM_SIZE=0x5000
 	CY_SEC_TOOLS_TARGET := cyb06xx7
 	# Set flash start and size
